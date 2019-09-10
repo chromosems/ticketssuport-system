dd9c84cf4e1b5f06fe257914d224cd42778d2e52
chromosems <opudaemmanuel@gmail.com>
2019-09-10 14:28:22 +0300

    all of it

---
+++ app/Http/Controllers/TicketsController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Ticket;
+use App\Category;
+
+class TicketsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $tickets = Ticket::paginate(10);
+        return view('tickets.index',compact('tickets'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        $categories = Category::all();
+        return view('tickets.create', compact('tickets'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, AppMailer $mailer)
+    {
+        //
+         $this->validate($request, [
+            'title' => 'required',
+            'category' => 'required',
+            'priority' => 'required',
+            'message' => 'required'
+        ]);
+ 
+        $ticket = new Ticket([
+            'title' => $request->input('title'),
+            'user_id' => Auth::user()->id,
+            'ticket_id' => strtoupper(str_random(10)),
+            'category_id' => $request->input('category'),
+            'priority' => $request->input('priority'),
+            'message' => $request->input('message'),
+            'status' => "Open"
+        ]);
+ 
+        $ticket->save();
+ 
+        $mailer->sendTicketInformation(Auth::user(), $ticket);
+ 
+        return redirect()->back()->with("status", "A ticket with ID: #$ticket->ticket_id has been opened.")
+
+    }
+    public function userTickets()
+    {
+        $tickets = Ticket::where('user_id',Auth::user()->id)->paginate(10);
+        return view('tickets.user_tickets',compact('tickets'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Ticket.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    //
+    protected $fillable =['user_id','caregory_id','ticket_id','priority','message','status'];
+
+    public function category()
+    {
+    	return $this->belongsTo(Category::class);
+    }
+
+
+    public function comments()
+    {
+    	return $this->hasMany(Comment::class);
+    }
+
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+    
+}
